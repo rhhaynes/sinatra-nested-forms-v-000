6a8294dff04644b1684c72ab7daec3f94d42373c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-10 16:01:25 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_reader :name, :type, :booty
+  SHIPS = []
+  
+  def self.all
+    SHIPS
+  end
+  
+  def self.clear
+    SHIPS = []
+  end
+  
+  def initialize()
+    SHIPS << self
+  end
 end
