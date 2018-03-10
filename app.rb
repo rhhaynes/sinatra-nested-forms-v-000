019d12bb197704d6b0e5d88882dd3cac2810081a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-10 18:02:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ module FormsLab
       erb :'pirates/new'
     end
     
+    get '/new' do
+    end
+    
     post '/pirates' do
       ship_params = params[:pirate].delete(:ships)
       ship_params.each{|attributes| Ship.new(attributes)}
