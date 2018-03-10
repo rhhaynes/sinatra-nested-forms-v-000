ff90528e296df93e54908fb146a9ab95b9c3e823
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-10 16:08:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
+    
+    get '/' do
+    end
+    
+    post '/pirates' do
+    end
 
   end
 end
